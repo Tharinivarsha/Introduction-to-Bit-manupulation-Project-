b194fbe96f23ee86675a97a1aa6a66adef325625
Tharinivarsha <chitraswamy11@gmail.com>
2024-10-16 18:02:41 +0530

    Introduction to Bit Manupulation(Project)

---
+++ main.py
@@ -0,0 +1,9 @@
+number1=10
+number2=18
+
+print(number1^number2,"This is Xor operator")
+print(number1|number2,"This is Or opertor")
+print(number1&number2,"This is And operator")
+print(~number1,"This is Not operator")
+print(number1<<1,"This is Left shift operator")
+print(number1>>1,"This is Right shift operator")
